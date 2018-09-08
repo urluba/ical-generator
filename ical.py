11e63d46cf3e54693deacd1bd4f0c3a36dc4bc66
Lucas Barrière <lucas.barriere@gmail.com>
2018-09-08 20:11:38 +0200

    Despair is back

--- ical.py
+++ ical.py
@@ -451,60 +451,60 @@ BUS_CALENDAR = [
             byday=['MO', 'TU', 'WE', 'TH', 'FR'],
         )
     ),
-    dict(
-        summary='roseraie',
-        start=(8, 57),
-        duration=timedelta(minutes=1),
-        location='roseraie',
-        rrule=dict(
-            byday=['MO', 'TU', 'WE', 'TH', 'FR'],
-        )
-    ),
-    dict(
-        summary='place du clos',
-        start=(12, 38, 2),
-        duration=timedelta(minutes=1),
-        location='place du clos',
-        rrule=dict(
-            byday=['WE'],
-        )
-    ),
-    dict(
-        summary='place du clos',
-        start=(15, 42),
-        duration=timedelta(minutes=1),
-        location='place du clos',
-        rrule=dict(
-            byday=['MO', 'TU', 'WE', 'TH', 'FR'],
-        )
-    ),
-    dict(
-        summary='place du clos',
-        start=(16, 42),
-        duration=timedelta(minutes=1),
-        location='place du clos',
-        rrule=dict(
-            byday=['MO', 'TU', 'WE', 'TH', 'FR'],
-        )
-    ),
-    dict(
-        summary='place du clos',
-        start=(17, 39),
-        duration=timedelta(minutes=1),
-        location='place du clos',
-        rrule=dict(
-            byday=['MO', 'TU', 'WE', 'TH', 'FR'],
-        )
-    ),
-    dict(
-        summary='place du clos',
-        start=(16, 45, 3),
-        duration=timedelta(minutes=1),
-        location='place du clos',
-        rrule=dict(
-            byday=['TH', 'FR'],
-        )
-    ),
+    # dict(
+    #     summary='roseraie',
+    #     start=(8, 57),
+    #     duration=timedelta(minutes=1),
+    #     location='roseraie',
+    #     rrule=dict(
+    #         byday=['MO', 'TU', 'WE', 'TH', 'FR'],
+    #     )
+    # ),
+    # dict(
+    #     summary='place du clos',
+    #     start=(12, 38, 2),
+    #     duration=timedelta(minutes=1),
+    #     location='place du clos',
+    #     rrule=dict(
+    #         byday=['WE'],
+    #     )
+    # ),
+    # dict(
+    #     summary='place du clos',
+    #     start=(15, 42),
+    #     duration=timedelta(minutes=1),
+    #     location='place du clos',
+    #     rrule=dict(
+    #         byday=['MO', 'TU', 'WE', 'TH', 'FR'],
+    #     )
+    # ),
+    # dict(
+    #     summary='place du clos',
+    #     start=(16, 42),
+    #     duration=timedelta(minutes=1),
+    #     location='place du clos',
+    #     rrule=dict(
+    #         byday=['MO', 'TU', 'WE', 'TH', 'FR'],
+    #     )
+    # ),
+    # dict(
+    #     summary='place du clos',
+    #     start=(17, 39),
+    #     duration=timedelta(minutes=1),
+    #     location='place du clos',
+    #     rrule=dict(
+    #         byday=['MO', 'TU', 'WE', 'TH', 'FR'],
+    #     )
+    # ),
+    # dict(
+    #     summary='place du clos',
+    #     start=(16, 45, 3),
+    #     duration=timedelta(minutes=1),
+    #     location='place du clos',
+    #     rrule=dict(
+    #         byday=['TH', 'FR'],
+    #     )
+    # ),
 ]
 
 class Planning(object):
@@ -726,8 +726,6 @@ def get_holidays_weeks(date_start: datetime, date_end: datetime) -> list:
 
 if __name__ == '__main__':
     bus_planning = WeeklyPlanning(
-        name='bus',
-        description='Calendrier des bus',
         events=BUS_CALENDAR,
         start=BUS_CALENDAR_START,
         end=BUS_CALENDAR_END,
